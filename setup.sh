168658ab9445857026e4c59f2273e3d79b1bd1d7
Elijah Buck <elijah.buck@gmail.com>
2021-09-20 16:47:34 -0700

    Delete setup.sh

--- setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Codespaces auth config
-cp .set_codespaces_token_from_gh_client ~/
-(grep -q 'source .set_codespaces_token_from_gh_client' ~/.bashrc || echo 'source ~/.set_codespaces_token_from_gh_client' >> ~/.bashrc) || true
