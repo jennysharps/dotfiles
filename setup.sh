307f6e35e42bcb2761b78015eb135b4f5d80ad3f
Jenny Sharps <jennysharps@github.com>
2023-02-06 09:35:05 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,28 +3,22 @@
 zshrc() {
     ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
     echo "\n==========================================================="
-    echo "             Installing plugins              "
-    echo "-----------------------------------------------------------"
+    echo "Installing plugins..."
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
-    echo "\n==========================================================="
-    echo "             Installing dracula theme               "
-    echo "-----------------------------------------------------------"
+    echo "Installing themes..."
     git clone https://github.com/dracula/zsh.git "$ZSH_CUSTOM/themes/dracula" --depth=1
     ln -s "$ZSH_CUSTOM/themes/dracula/dracula.zsh-theme" "$ZSH_CUSTOM/themes/dracula.zsh-theme"
-    echo "\n==========================================================="
-    echo "                  Import zshrc                             "
-    echo "-----------------------------------------------------------"
+    echo "Importing zshrc..."
     cat .zshrc > $HOME/.zshrc
 }
 
 echo "\n==========================================================="
-echo "             Setting up GITHUB_TOKEN                       "
-echo "-----------------------------------------------------------"
+echo "Setting up dotfiles..."
+
+# Set up GITHUB_TOKEN
 # Use GH_PAT instead of default GITHUB_TOKEN if it is defined.
 [ -v GH_GH_PAT ] && export GITHUB_TOKEN="${GH_GH_PAT}"
 
-echo "\n==========================================================="
-echo "             Setting up ZSH                                "
-echo "-----------------------------------------------------------"
+# Set up zshrc
 zshrc
