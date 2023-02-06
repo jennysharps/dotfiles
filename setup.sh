432a8f397abd58683f36415cdd5e74f0f91b904b
Jenny Sharps <jennysharps@github.com>
2023-02-06 09:26:36 -0500

    fix line spacing

--- setup.sh
+++ setup.sh
@@ -2,29 +2,29 @@
 
 zshrc() {
     ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
-    echo "==========================================================="
+    echo "\n==========================================================="
     echo "             Installing plugins              "
     echo "-----------------------------------------------------------"
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
-    echo "==========================================================="
+    echo "\n==========================================================="
     echo "             Installing dracula theme               "
     echo "-----------------------------------------------------------"
     git clone https://github.com/dracula/zsh.git "$ZSH_CUSTOM/themes/dracula" --depth=1
     ln -s "$ZSH_CUSTOM/themes/dracula/dracula.zsh-theme" "$ZSH_CUSTOM/themes/dracula.zsh-theme"
-    echo "==========================================================="
+    echo "\n==========================================================="
     echo "                  Import zshrc                             "
     echo "-----------------------------------------------------------"
     cat .zshrc > $HOME/.zshrc
 }
 
-echo "==========================================================="
+echo "\n==========================================================="
 echo "             Setting up GITHUB_TOKEN                       "
 echo "-----------------------------------------------------------"
 # Use GH_PAT instead of default GITHUB_TOKEN if it is defined.
 [ -v GH_GH_PAT ] && export GITHUB_TOKEN="${GH_GH_PAT}"
 
-echo "==========================================================="
+echo "\n==========================================================="
 echo "             Setting up ZSH                                "
 echo "-----------------------------------------------------------"
 zshrc
