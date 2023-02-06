b7027d55a0905695762f4c5443d8f9c3df26b499
Jenny Sharps <jennysharps@github.com>
2023-02-06 10:43:19 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,14 +2,24 @@
 
 zshrc() {
     ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+    
     echo "Installing plugins..."
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
+    
     echo "Installing themes..."
     git clone https://github.com/dracula/zsh.git "$ZSH_CUSTOM/themes/dracula" --depth=1
     ln -s "$ZSH_CUSTOM/themes/dracula/dracula.zsh-theme" "$ZSH_CUSTOM/themes/dracula.zsh-theme"
+    
     echo "Importing zshrc..."
     cat .zshrc > $HOME/.zshrc
+    
+    echo "Setting up brew & rbenv..."
+    # From https://github.com/github/codespaces-base/blob/main/image/.devcontainer/Dockerfile#LL257C3-L257C57
+    # Setup Homebrew
+    # This is done in the `.devcontainer` for .bashrc, but not for .zshrc
+    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> "${HOME}/.zshrc"
+    echo 'eval "$(rbenv init -)"' >> "${HOME}/.zshrc"
 }
 
 echo "Setting up dotfiles..."
