8449288ca89e2f1d8bdf06cbdc723ac398e6f581
Jenny Sharps <jennysharps@github.com>
2023-02-06 09:40:10 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@
 
 zshrc() {
     ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
-    echo "\n==========================================================="
     echo "Installing plugins..."
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
@@ -13,12 +12,11 @@ zshrc() {
     cat .zshrc > $HOME/.zshrc
 }
 
-echo "\n==========================================================="
 echo "Setting up dotfiles..."
 
 # Set up GITHUB_TOKEN
 # Use GH_PAT instead of default GITHUB_TOKEN if it is defined.
 [ -v GH_GH_PAT ] && export GITHUB_TOKEN="${GH_GH_PAT}"
 
-# Set up zshrc
+echo "Setting up ZSH..."
 zshrc
